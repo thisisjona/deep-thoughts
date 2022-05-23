6cba6df75daa544cff0829bec34376febed357a8
thisisjona <thisisjona@gmail.com>
2022-05-22 20:37:41 -0500

    integrated client

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const db = require('./config/connection');
 
 const { authMiddleware } = require('./utils/auth');
 
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
@@ -26,6 +28,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
 await server.start();
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
+
+//Serve up static assets
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
     app.listen(PORT, () => {
